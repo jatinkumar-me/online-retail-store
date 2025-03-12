ab028d62765c5d7fad6830710ee58d0e1144ef52
Jatin Kumar <jatin208015@gmail.com>
2025-03-12 10:36:23 +0530

    add more request mappings

--- customer/src/main/java/com/jatin/customer/controller/CustomerController.java
+++ customer/src/main/java/com/jatin/customer/controller/CustomerController.java
@@ -2,11 +2,13 @@ package com.jatin.customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jatin.customer.dto.CustomerDto;
@@ -14,26 +16,33 @@ import com.jatin.customer.entity.Customer;
 import com.jatin.customer.service.CustomerService;
 
 @RestController
+@RequestMapping("/api")
 public class CustomerController {
 
 	@Autowired
 	private CustomerService customerService;
 
-	@PostMapping("/customer")
+	@PostMapping("/addCustomer")
 	public ResponseEntity<Customer> createCustomer(@RequestBody CustomerDto customerDto) {
 		Customer savedCustomer = customerService.saveCustomer(customerDto);
 		return ResponseEntity.ok(savedCustomer);
 	}
 
-	@GetMapping("/customer/{customerId}")
+	@GetMapping("/searchCustomer")
 	public ResponseEntity<Customer> getCustomer(@PathVariable Long customerId) {
 		Customer customer = customerService.getCustomerById(customerId);
 		return ResponseEntity.ok(customer);
 	}
 
-	@PutMapping("/customer/{customerId}")
+	@PutMapping("/updateCustomer")
 	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer) {
 		Customer updatedCustomer = customerService.updateCustomer(customer);
 		return ResponseEntity.ok(updatedCustomer);
+	}
+
+	@DeleteMapping("/deleteCustomer")
+	public ResponseEntity<String> deleteCustomer(@PathVariable Long customerId) {
+		customerService.deleteCustomer(customerId);
+		return ResponseEntity.ok("Customer deleted successfully");
 	}
 }
